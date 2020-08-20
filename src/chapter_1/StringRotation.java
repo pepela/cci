60aca79b7abbbbaf7038b85c0763edbc23670a75
Giorgi <giorgi.peranidze@gmail.com>
2020-08-20 16:13:00 +0300

    String rotation

---
+++ src/chapter_1/StringRotation.java
@@ -0,0 +1,12 @@
+package chapter_1;
+
+public class StringRotation {
+
+    public boolean solve(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        return (s1 + s1).contains(s2);
+    }
+}
